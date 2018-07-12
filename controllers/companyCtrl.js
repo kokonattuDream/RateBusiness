d909ec152419ff2a1616c16a463ae21a2e3c703f
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-12 19:16:52 -0400

    error handling

--- controllers/companyCtrl.js
+++ controllers/companyCtrl.js
@@ -3,6 +3,16 @@ const Company = require('../models/company');
 
 exports.createCompany = async (req, res) => {
 
+    if(req.body.name === undefined || req.body.address === undefined || req.body.city === undefined ||
+        req.body.country === undefined || req.body.sector === undefined || req.body.website === undefined){
+        return res.status(200).json({error: 'Cannot create company with empty fields'});
+    }
+
+    if(req.body.name === '' || req.body.address === '' || req.body.city === '' ||
+        req.body.country === '' || req.body.sector === '' || req.body.website === ''){
+        return res.status(200).json({error: 'Cannot submit empty fields'});
+    }
+
     const newCompany = new Company();
 
     newCompany.companyname = req.body.name;
