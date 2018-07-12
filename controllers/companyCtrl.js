a88bb7fcc2e0ec76b9f39e0528d57fd0390ad387
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-12 19:04:09 -0400

    save company

--- controllers/companyCtrl.js
+++ controllers/companyCtrl.js
@@ -1,6 +1,20 @@
 const User = require('../models/user');
 const Company = require('../models/company');
 
-exports.createCompany = (req, res) => {
-    
+exports.createCompany = async (req, res) => {
+
+    const newCompany = new Company();
+
+    newCompany.companyname = req.body.name;
+    newCompany.address = req.body.address;
+    newCompany.city = req.body.city;
+    newCompany.country = req.body.country;
+    newCompany.sector = req.body.sector;
+    newCompany.website = req.body.website;
+    newCompany.admin = req.body.userId;
+
+    const company = await newCompany.save();
+
+    return res.status(200).json({message: 'Company created successfully'});
+
 }
