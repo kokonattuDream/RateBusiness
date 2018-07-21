499c8f0d411cf5ec85645248ce5eff501e10de9d
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-21 09:51:41 -0400

    upload image

--- controllers/fileCtrl.js
+++ controllers/fileCtrl.js
@@ -9,5 +9,21 @@ cloudinary.config({
 });
 
 exports.addImage = async (req, res) =>{
+    cloudinary.uploader.upload(req.body.image, (result) => {
+        const savedData = async() => {
+            if(req.body.image){
+                await User.update({
+                    '_id':req.body.user._id
+                },{
+                    "imageId": result.public_id,
+                    "imageVersion": result.version
+                });
+            }
+        }
 
+        savedData()
+            .then(res => {
+                return res.status(200).json({message: 'Profile image upload'});
+            })
+    });
 }
